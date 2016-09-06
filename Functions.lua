bf48d2387aef335210d93178d13586ad09f4d69d
ElGiovanni <giovanni.lopez3523@gmail.com>
2016-09-05 20:01:43 -0700

    Add files via upload

---
+++ Functions.lua
@@ -0,0 +1,51 @@
+function sin(x)
+  return math.sin(x)
+end
+
+function cos(x)
+  return math.cos(x)
+end
+
+function tan(x)
+  return math.tan(x)
+end
+
+function arcsen(x)
+  return math.asin(x)
+end
+
+function arccos(x)
+  return math.acos(x)
+end
+
+function arctan(x)
+  return math.atan(x)
+end
+
+function sinh(x)
+  return math.sinh(x)
+end
+
+function cosh(x)
+  return math.cosh(x)
+end
+
+function tanh(x)
+  return math.tanh(x)
+end
+
+function ln(x)
+  return math.log(x)
+end
+
+function log10(x)
+  return math.log10(x)
+end
+
+function exp(x)
+  return math.exp(x)
+end
+
+function sqrt(x)
+  return math.sqrt(x)
+end
---
+++ estabilidad1.lua
@@ -0,0 +1,51 @@
+--[[
+	Programa: Estabilidad1.lua
+	Autor: GLC
+	Fecha:	2/09/2016
+	Descripcion: Aproximaciones con un procedimiento estable
+]]--
+
+-- ***************************** FUNCIONES *******************************
+X0, X1 = 1, (1/3)
+
+--Redondeo
+function Round(x,n)
+    return ("%.".. n .."g"):format(x)  --[[ Convierte un numero punto-flotante a decimal o notacion decimal exponencial dependiendo del valor y la precision,
+											donde la precision se define por un . seguido por un numero entero o un asterisco ]]
+end
+--Calcula la aproximacion
+function FunEstable(n)
+	local c1, c2 = 1, (2/3)
+	return c1 + c2*n
+end
+--Calcula el error
+function Error(ValV, ValA)
+	return (math.abs((ValV-ValA)/ValV))
+end
+
+-- ***************************** MAIN *****************************
+print"Este programa calculara aproximaciones al valor Xn por un procedimiento estable, con aritmetica de redondeo a k cifras."
+io.write("Cual es su nombre? ")
+nombre = io.read()
+io.write(nombre..", Dame el numero de iteraciones: ")
+x = io.read()
+io.write"Dame el numero de cifras significativas: "
+n = io.read()
+
+s = string.format("%30s%30s%30s%30s%30s","| Xn","| fl(Xn) ","| ErrorV ","| ErrorR","| ErrorRPorcentual")
+print("| n " .. s)
+s1 = string.format("%29s%25s%30s%41s%20s"," | " .. X0," | " .. Round(X0,n)," | " .. Error(X0,Round(X0,n))," | -----------","| -----------")
+print("| 1 " .. s1)
+s2 = string.format("%44s%16s%42s%24s%20s"," | " .. X1," | " .. Round(X1,n)," | " .. Error(X1,Round(X1,n))," | " .. Error(Round(X1,n),Round(X0,n))," | " .. Error(Round(X1,n),Round(X0,n))*100)
+print("| 2 " .. s2)
+B = X1
+for i = 2, x do
+	local a = FunEstable(i)
+	local b = Round(a,n)
+	local c = Round(Error(a,b),n)
+	local d = Error(a,B)
+	local e = d*100
+  s3 = string.format("%44s%16s%35s%36s%19s"," | " .. a," | " .. b," | " .. c," | " .. d," | " .. e)
+	print("| " .. i .. s3) -- Lo imprime bien feo profe :C
+	B = a
+end
