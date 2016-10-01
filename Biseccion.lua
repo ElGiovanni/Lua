492118f03b1aaf9b60dd564354c0dd86055e4677
Giovanni Lopez Celaya <giovanni.lopez3523@gmail.com>
2016-09-30 18:02:37 -0700

    Add files via upload

--- Biseccion.lua
+++ Biseccion.lua
@@ -1,56 +1,342 @@
+--[[
+  Programa: Biseccion.lua
+  Autor: GLC
+  Fecha: 5/09/2016
+  Descripcion: Aproximaciones al valor de x con metodo de biseccion
+]]
+
 require "Functions"
 
 -- ******************************** FUNCTIONS *************************************
-function leer()
+function Combinacion()
   os.execute("cls")
-  io.write("Porfavor deme la ecuacion que quiera usar para la aproximacion: ")
+  x = 0
+  io.write("Por favor dame la ecuacion que quiera usar para la aproximacion: ")
   local s = io.stdin:read()
-  s = "x=" .. s
+  s = "a=" .. s
+  io.write("Por favor dame el primer valor del intervalo [a]: ")
+  local an = io.read()
+  io.write("Por favor dame el segundo valor del intervalo [b]: ")
+  local bn = io.read()
+  io.write("Por favor deme el n".. string.char(163) .."mero de la TOL aceptada (solo el valor de k): ")
+  local n = io.read()
+  TOL = 5*math.pow(10,-n)
+
+  pn = ((an+bn)/2)
+  x = an
   loadstring(s)()
-  print(x)
+  fa = a
+  x = pn
+  loadstring(s)()
+  fp = a
+  s1 = string.format("%-8s%-20s%-20s%-20s%-25s%-25s%-25s","|n","|an","|bn","|pn","|f(an)","|f(pn)","|error")
+  print(s1)
+  s2 = string.format("%-8s%-20s%-20s%-20s%-25s%-25s%-25s","|0","|" .. an,"|" .. bn,"|" .. pn,"|" .. fa,"|" .. fp,"|  ------")
+  print(s2)
+
+  if fa*fp > 0 then
+      an = pn
+  else
+      bn = pn
+  end
+  pn2 = pn
+
+  i = 1
+  repeat
+    pn = ((an+bn)/2)
+    x = an
+    loadstring(s)()
+    fa = a
+    x = pn
+    loadstring(s)()
+    fp = a
+    local Error = math.abs((pn-pn2)/pn)
+    s2 = string.format("%-8s%-20s%-20s%-20s%-25s%-25s%-25s","|" .. i,"|" .. an,"|" .. bn,"|" .. pn,"|" .. fa,"|" .. fp,"|" .. Error)
+    print(s2)
+    if fa*fp > 0 then
+        an = pn
+    else
+        bn = pn
+    end
+    pn2 = pn
+    i = i+1
+  until Error < TOL
   os.execute("pause")
 end
 
-function Polinomial()
+-- ******************************** POLINOMIO ********************************
+function PoliEva(a,n,x)
+  local res = 0.0
+  for i=0,n do
+      res = res + (a*math.pow(x,n))
+  end
+  return res
+end
 
+function Polinomial()
+  os.execute("cls")
+  tablaA = {}
+  io.write("Por favor deme el numero de coeficientes del polinomio: ")
+  local n = io.read("*n")
+  io.write("Por favor deme el valor de x: ")
+  local x = io.read("*n")
+  io.write("Por favor deme el intervalo [a,b]: ")
+  local a,b = io.read("*n","*n")
+
+  s = string.format("%-8s%-20s%-20s%-20s%-25s%-25s%-25s","|n","|an","|bn","|pn","|f(an)","|f(pn)","|error")
+  print(s)
+
+  for j=0,n do
+    s1 = string.format("%-8s%-20s%-20s%-20s%-25s%-25s%-25s","|" .. j,"|" .. a,"|" .. b,"|" .. ((a+b)/2),"|" .. PoliEva(a,n,x),"|f(pn)","|error")
+    print(s1)
+  end
+  os.execute("pause")
+end
+
+-- ******************************** EXPONENCIAL ********************************
+function Expo(a,b,c,k,x)
+  return a*math.exp(k*x+c)+b
 end
 
 function Exponencial()
+  os.execute("cls")
+  io.write("Por favor deme el n".. string.char(163) .."mero de la TOL aceptada (solo el valor de k): ")
+  local n = io.read("*n")
+  local TOL = 5*math.pow(10,-n)
+  io.write("Por favor deme el valor de la constante a: ")
+  local a = io.read("*n")
+  io.write("Por favor deme el valor de la constante k: ")
+  local k = io.read("*n")
+  io.write("Por favor deme el valor de la constante c: ")
+  local c = io.read("*n")
+  io.write("Por favor deme el valor de la constante b: ")
+  local b = io.read("*n")
+  io.write("Por favor deme el intervalo [a,b]: ")
+  local an,bn = io.read("*n","*n")
 
+  s = string.format("%-8s%-20s%-20s%-20s%-25s%-25s%-25s","|n","|an","|bn","|pn","|f(an)","|f(pn)","|error")
+  print(s)
+  local pn = ((an+bn)/2)
+  local fa = Expo(a,b,c,k,an)
+  local fp = Expo(a,b,c,k,pn)
+  s2 = string.format("%-8s%-20s%-20s%-20s%-25s%-25s%-25s","|0","|" .. an,"|" .. bn,"|" .. pn,"|" .. fa,"|" .. fp,"|  ------")
+  print(s2)
+  if fa*fp > 0 then
+      an = pn
+  else
+      bn = pn
+  end
+  pn2 = pn
+
+  i = 1
+  repeat
+    local pn = ((an+bn)/2)
+    local fa = Expo(a,b,c,k,an)
+    local fp = Expo(a,b,c,k,pn)
+    local Error = math.abs((pn-pn2)/pn)
+    s2 = string.format("%-8s%-20s%-20s%-20s%-25s%-25s%-25s","|" .. i,"|" .. an,"|" .. bn,"|" .. pn,"|" .. fa,"|" .. fp,"|" .. Error)
+    print(s2)
+    if fa*fp > 0 then
+        an = pn
+    else
+        bn = pn
+    end
+    pn2 = pn
+    i = i+1
+  until Error < TOL
+  os.execute("pause")
+end
+
+-- ******************************** LOGARITMICA ********************************
+function LogEva(a,b,c,k,x,val)
+  if val == '1' then
+    return a*math.log(k*x+c)+b
+  else
+    return a*math.log10(k*x+c)+b
+  end
 end
 
 function Logaritmica()
+  os.execute("cls")
+  io.write("Por favor deme el n".. string.char(163) .."mero de la TOL aceptada (solo el valor de k): ")
+  local n = io.read("*n")
+  local TOL = 5*math.pow(10,-n)
+  io.write("Por favor deme el valor de la constante a: ")
+  local a = io.read("*n")
+  io.write("Por favor deme el valor de la constante k: ")
+  local k = io.read("*n")
+  io.write("Por favor deme el valor de la constante c: ")
+  local c = io.read("*n")
+  io.write("Por favor deme el valor de la constante b: ")
+  local b = io.read("*n")
+  io.write("Por favor deme el intervalo [a,b]: ")
+  local an,bn = io.read("*n","*n")
 
+  print("Cual funci".. string.char(162) .."n desea utilizar?")
+  print("1) Ln")
+  print("2) Log")
+  repeat
+    local val = io.read()
+    if val == '1' then
+      r = '1'
+    else
+      r = '2'
+    end
+  until val >= '1' and val <= '2'
+
+  s = string.format("%-8s%-20s%-20s%-20s%-25s%-25s%-25s","|n","|an","|bn","|pn","|f(an)","|f(pn)","|error")
+  print(s)
+  local pn = ((an+bn)/2)
+  local fa = LogEva(a,b,c,k,an,r)
+  local fp = LogEva(a,b,c,k,pn,r)
+  s2 = string.format("%-8s%-20s%-20s%-20s%-25s%-25s%-25s","|0","|" .. an,"|" .. bn,"|" .. pn,"|" .. fa,"|" .. fp,"|  ------")
+  print(s2)
+  if fa*fp > 0 then
+      an = pn
+  else
+      bn = pn
+  end
+  pn2 = pn
+
+  i = 1
+  repeat
+    local pn = ((an+bn)/2)
+    local fa = LogEva(a,b,c,k,an,r)
+    local fp = LogEva(a,b,c,k,pn,r)
+    local Error = math.abs((pn-pn2)/pn)
+    s2 = string.format("%-8s%-20s%-20s%-20s%-25s%-25s%-25s","|" .. i,"|" .. an,"|" .. bn,"|" .. pn,"|" .. fa,"|" .. fp,"|" .. Error)
+    print(s2)
+    if fa*fp > 0 then
+        an = pn
+    else
+        bn = pn
+    end
+    pn2 = pn
+    i = i+1
+  until Error < TOL
+  os.execute("pause")
+end
+
+-- ******************************** TRIGONOMETRICA ********************************
+function Trig (a,b,c,k,x,val)
+  if val == '1' then
+    return a*math.sin(k*x+c)+b
+  elseif val == '2' then
+    return a*math.cos(k*x+c)+b
+  elseif val == '3' then
+    return a*math.tan(k*x+c)+b
+  elseif val == '4' then
+    return a*math.asin(k*x+c)+b
+  elseif val == '5' then
+    return a*math.acos(k*x+c)+b
+  elseif val == '6' then
+    return a*math.atan(k*x+c)+b
+  elseif val == '7' then
+    return a*math.sinh(k*x+c)+b
+  elseif val == '8' then
+    return a*math.cosh(k*x+c)+b
+  else
+    return a*math.tanh(k*x+c)+b
+  end
 end
 
 function Trigonometrica()
+  os.execute("cls")
+  io.write("Por favor deme el n".. string.char(163) .."mero de la TOL aceptada (solo el valor de k): ")
+  local n = io.read("*n")
+  local TOL = 5*math.pow(10,-n)
+  io.write("Por favor deme el valor de la constante a: ")
+  local a = io.read("*n")
+  io.write("Por favor deme el valor de la constante k: ")
+  local k = io.read("*n")
+  io.write("Por favor deme el valor de la constante c: ")
+  local c = io.read("*n")
+  io.write("Por favor deme el valor de la constante b: ")
+  local b = io.read("*n")
+  io.write("Por favor deme el intervalo [a,b]: ")
+  local an,bn = io.read("*n","*n")
 
-end
-
-function printTable(t, length)
-    for _,row in pairs(t) do
-        local format = ""
-        for i=1,#row do format = format .. "%" .. length .. "d" end
-        print(string.format(format, table.unpack(row)))
+  print("Cual funci".. string.char(162) .."n desea utilizar?")
+  print("1) Sen")
+  print("2) Cos")
+  print("3) Tan")
+  print("4) ArcSen")
+  print("5) ArcCos")
+  print("6) ArcTan")
+  print("7) Senh")
+  print("8) Cosh")
+  print("9) Tanh")
+  repeat
+    local val = io.read()
+    if val == '1' then
+      r = '1'
+    elseif val == '2' then
+      r = '2'
+    elseif val == '3' then
+      r = '3'
+    elseif val == '4' then
+      r = '4'
+    elseif val == '5' then
+      r = '5'
+    elseif val == '6' then
+      r = '6'
+    elseif val == '7' then
+      r = '7'
+    elseif val == '8' then
+      r = '8'
+    else
+      r = '9'
     end
-end
+  until val >= '1' and val <= '9'
 
+  s = string.format("%-8s%-20s%-20s%-20s%-25s%-25s%-25s","|n","|an","|bn","|pn","|f(an)","|f(pn)","|error")
+  print(s)
+  local pn = ((an+bn)/2)
+  local fa = Trig(a,b,c,k,an,r)
+  local fp = Trig(a,b,c,k,pn,r)
+  s2 = string.format("%-8s%-20s%-20s%-20s%-25s%-25s%-25s","|0","|" .. an,"|" .. bn,"|" .. pn,"|" .. fa,"|" .. fp,"|  ------")
+  print(s2)
+  if fa*fp > 0 then
+      an = pn
+  else
+      bn = pn
+  end
+  pn2 = pn
+
+  i = 1
+  repeat
+    local pn = ((an+bn)/2)
+    local fa = Trig(a,b,c,k,an,r)
+    local fp = Trig(a,b,c,k,pn,r)
+    local Error = math.abs((pn-pn2)/pn)
+    s2 = string.format("%-8s%-20s%-20s%-20s%-25s%-25s%-25s","|" .. i,"|" .. an,"|" .. bn,"|" .. pn,"|" .. fa,"|" .. fp,"|" .. Error)
+    print(s2)
+    if fa*fp > 0 then
+        an = pn
+    else
+        bn = pn
+    end
+    pn2 = pn
+    i = i+1
+  until Error < TOL
+  os.execute("pause")
+end
 
 -- ************************************ MAIN **************************************
 repeat
   os.execute("cls")
-  print("Este programa encontrara aproximaciones a la raiz utilizando el metodo de biseccion ")
-  print("Eliga una de las opciones de como hallar la aproximacion")
+  print(string.char(144) .."ste programa encontrara aproximaciones a la ra".. string.char(161) .."z utilizando el m".. string.char(130) .."todo de bisecci".. string.char(162) .."n ")
+  print("Eliga una de las opciones de como hallar la aproximaci".. string.char(162) .."n")
   print()
   print("1) Polinomial:  f(x) = a0 + a1 x +  a2 x2 + ... + an xn")
   print("2) Exponencial: f(x) = a e^(k x + c) + b")
-  print("3) Logaritmica: f(x) =  a Ω(k x + c) + b    Ω = log10, ln")
-  print("4) Trigonometrica: f(x) = a Ω(k x + c) + b  Ω = sen, cos, etc")
+  print("3) Logaritmica: f(x) =  a ".. string.char(225) .."(k x + c) + b    ".. string.char(225) .." = log10, ln")
+  print("4) Trigonom".. string.char(130) .."trica: f(x) = a ".. string.char(225) .."(k x + c) + b  ".. string.char(225) .." = sen, cos, etc")
   print("5) Combinacion de las anteriores")
   print("6) Salir")
   repeat
     resp = io.read()
-  until resp >= '1' or resp <= '6'
+  until resp >= '1' and resp <= '6'
   if resp == '1' then
     Polinomial()
   elseif resp == '2' then
@@ -60,7 +346,7 @@ repeat
   elseif resp == '4' then
     Trigonometrica()
   elseif resp == '5' then
-    leer()
+    Combinacion()
   end
 until resp == '6'
 
---
+++ PuntoFijo.lua
@@ -0,0 +1,74 @@
+require "Functions"
+
+os.execute("cls")
+x = 0
+print("Este programa calculara aproximaciones a x utilizando el metodo de punto fijo.")
+io.write("Por favor dame la funcion que quiera utilizar: ")
+local s = io.stdin:read()
+local f = "a=" .. s
+io.write("Por favor dame la funcion despejada: ")
+s = io.stdin:read()
+local g = "b=" .. s
+io.write("Por favor dame el primer valor del intervalo [a,b]: ")
+local an,bn = io.read("*n","*n")
+repeat
+  io.write("Por favor dame el numero de iteraciones: ")
+  iter = io.read("*n")
+  if iter <= 0 then print("Ingrese un numero mayor que 0: ") end
+until iter >= 0
+io.write("Por favor deme el n".. string.char(163) .."mero de la TOL aceptada (solo el valor de k): ")
+local n = io.read("*n")
+TOL = 5*math.pow(10,-n)
+
+x = an
+loadstring(f)()
+fa = a
+x = bn
+loadstring(f)()
+fb = a
+pn = (an+bn)/2
+x = pn
+loadstring(f)()
+fp = a
+
+error = 100
+cont = 1
+if fa * fb < 0 then
+  tabla = string.format("%-8s%-20s%-25s%-20s","|n","|pn","|fp","|Error")
+  print(tabla)
+  tabla = string.format("%-8s%-20s%-25s%-20s","|0","|" .. pn,"|" .. fp,"| -------")
+  print(tabla)
+
+  while error > TOL and iter > cont do
+    ant = pn
+    x = an
+    loadstring(f)()
+    fa = a
+    x = bn
+    loadstring(f)()
+    fb = a
+    x = ant
+    loadstring(g)()
+    pn = b
+    x = pn
+    loadstring(f)()
+    fp = a
+    error = math.abs((x - ant)/x)
+    tabla = string.format("%-8s%-20s%-25s%-2s","|" .. cont,"|" .. pn,"|" .. fp,"|" .. error)
+    print(tabla)
+    cont = cont + 1
+    if fp == 0 or error <= TOL then
+      print()
+      print("la raiz tolerable es: " .. pn)
+    end
+  end
+else
+  print()
+  print("No hay cambio de signo al evaluar en los intervalos dado,")
+  print("por lo tanto no podemos hacer aprox. a la ra".. string.char(161) .."z")
+end
+if cont > iter then
+  print("no converge en ".. iter .." iteraciones")
+end
+os.execute("pause")
+os.execute("cls")
